b9ea3d8c9c9afeaa4165d51b93114c7682af1c81
senel34 <joshuapohan93@gmail.com>
2019-05-04 23:06:34 +0700

    fixed bash script copy client build

--- release_script.sh
+++ release_script.sh
@@ -2,4 +2,4 @@ python create_tables.py
 cd static/board-client
 npm install --only=dev && npm install && npm run build
 cp -R static/board-client/build/static/. static/
-cp static/board-client/build/. static/
+cp static/board-client/build/* static/
