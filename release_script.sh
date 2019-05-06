5bb8300b6e65f1bc809fe7dac40401a94d42c39f
senel34 <joshuapohan93@gmail.com>
2019-05-06 22:26:01 +0700

    fix build command

--- release_script.sh
+++ release_script.sh
@@ -1,6 +1,4 @@
-cd static/board-client
-npm install --only=dev && npm install && npm run build
 cd ../../
-cp -R static/board-client/build/static/. static/ 2> /dev/null
-cp -R static/board-client/build/. static/ 2> /dev/null
+cp -R static/board-client/build/static/. static/
+cp -R static/board-client/build/. static/
 ls static
