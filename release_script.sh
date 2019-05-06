5f2c899faf271fd549664ebf2c079091576495c7
senel34 <joshuapohan93@gmail.com>
2019-05-06 20:42:44 +0700

    fix bash script

--- release_script.sh
+++ release_script.sh
@@ -2,3 +2,4 @@ cd static/board-client
 npm install --only=dev && npm install && npm run build
 cd ../../
 cp -R static/board-client/build/static/. static/ 2> /dev/null
+cp static/board-client/build/. static/ 2> /dev/null
