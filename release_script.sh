24ab3e5389bb7052df84e254891c6ae808a4012f
senel34 <joshuapohan93@gmail.com>
2019-05-06 19:51:35 +0700

    fixed bash script

--- release_script.sh
+++ release_script.sh
@@ -1,5 +1,5 @@
-python create_tables.py
 cd static/board-client
 npm install --only=dev && npm install && npm run build
+cd ../../
 cp -R static/board-client/build/static/. static/
 cp static/board-client/build/* static/
