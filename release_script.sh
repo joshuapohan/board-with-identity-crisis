921dea70b0b461f604a7ee4ab3babd3b11622117
senel34 <joshuapohan93@gmail.com>
2019-05-06 19:32:16 +0700

    build client

--- release_script.sh
+++ release_script.sh
@@ -1,1 +1,5 @@
 python create_tables.py
+cd static/board-client
+npm install --only=dev && npm install && npm run build
+cp -R static/board-client/build/static/. static/
+cp static/board-client/build/* static/
