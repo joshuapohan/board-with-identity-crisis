59d71ed18a61ab2b15d66e9b968328d6b4655a1a
senel34 <joshuapohan93@gmail.com>
2019-05-06 20:53:33 +0700

    test

--- release_script.sh
+++ release_script.sh
@@ -3,3 +3,4 @@ npm install --only=dev && npm install && npm run build
 cd ../../
 cp -R static/board-client/build/static/. static/ 2> /dev/null
 cp -R static/board-client/build/. static/ 2> /dev/null
+ls static
