331b0ae35cb0a33cd1f6792d16cc9c10c3fad69b
senel34 <joshuapohan93@gmail.com>
2019-05-06 21:44:46 +0700

    ttttt

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 from config import Config
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-app = Flask(__name__,static_folder=os.path.join(basedir,'static'))
+app = Flask(__name__,static_folder=os.path.join('.','static'))
 Config.load_config()
 
 import router
