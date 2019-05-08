4fc148b3be89d0547051ceab019ae76f282f0db0
senel34 <joshuapohan93@gmail.com>
2019-05-08 23:02:29 +0700

    user table migrate

--- create_tables.py
+++ create_tables.py
@@ -22,6 +22,9 @@ try:
 
     create_table = "CREATE TABLE IF NOT EXISTS task_config(task_id INTEGER, color_id INTEGER)"
     cursor.execute(create_table)
+
+    create_table = "DROP TABLE IF EXISTS users"
+    cursor.execute(create_table)
     
     create_table = "CREATE TABLE IF NOT EXISTS users(id serial PRIMARY KEY, username VARCHAR(255), hashed_pw VARCHAR(256), email VARCHAR(255), is_validated BOOLEAN)"
     cursor.execute(create_table)
